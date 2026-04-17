e8dec50ea6bfde8d049d7443da4c8d844e9c75a1
MRashmimaiya <126222433+MRashmimaiya@users.noreply.github.com>
2026-04-17 12:34:41 +0530

    Update covid_data.py

--- covid_data.py
+++ covid_data.py
@@ -1,21 +1,3 @@
-# dictionary:
-# data={"name":"John","age":25}
-# print(data["name"])
-
-# list comprehension:
-# squares=[x**2 for x in range(5)]
-# print(squares)
-
-# append() and extend():
-# a=[1,2]
-# a.append(3)
-# print(a)
-
-# numpy:
-# import numpy as np
-# arr=np.array([1,2,3])
-# print(arr)
-
 # pandas:
 import pandas as pd
 df=pd.read_csv("covid_data_project.csv")
