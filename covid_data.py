4d4b2740eea50835258a32d4bc9a771070ba45d3
MRashmimaiya <126222433+MRashmimaiya@users.noreply.github.com>
2026-04-17 12:35:07 +0530

    Update covid_data.py

--- covid_data.py
+++ covid_data.py
@@ -4,10 +4,10 @@ df=pd.read_csv("covid_data_project.csv")
 print(df)
 
 # missing values:
-# import pandas as pd
-# df=pd.read_csv('covid_data_project.csv')
-# df.dropna()
-# df.fillna(0)
-# print(df.loc[0,"date"])
-# print(df.iloc[0,1])
+import pandas as pd
+df=pd.read_csv('covid_data_project.csv')
+df.dropna()
+df.fillna(0)
+print(df.loc[0,"date"])
+print(df.iloc[0,1])
 
